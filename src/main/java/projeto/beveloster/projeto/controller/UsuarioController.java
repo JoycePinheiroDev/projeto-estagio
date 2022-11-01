52530f49cfde6257f0684289bf9a45cdee6f08f9
Joyce Pinheiro <joyce.real01@gmail.com>
2022-11-01 10:10:59 -0300

    Adicionando método deletar na classe usuário

--- src/main/java/projeto/beveloster/projeto/controller/UsuarioController.java
+++ src/main/java/projeto/beveloster/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import projeto.beveloster.projeto.model.Produto;
 import projeto.beveloster.projeto.model.Usuario;
 import projeto.beveloster.projeto.repository.UsuarioRepository;
 
@@ -37,6 +38,17 @@ public class UsuarioController {
     @PostMapping()
     public ResponseEntity<Usuario> saveUsuario (@RequestBody Usuario usuario){
         return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+
+    // Detetar um usuario
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable(value = "id") Integer id){
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if (usuarioOptional.isPresent()){
+            usuarioRepository.delete(usuarioOptional.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
